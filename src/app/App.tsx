4765f229776a33b17c9f09eeb24672e027c9089e
astropsy999 <bareust@gmail.com>
2022-09-14 22:15:56 +0300

    FSD sorting files and folders + absolute paths

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, {Suspense, useContext, useState} from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
-import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
-import {useTheme} from './theme/useTheme';
-import {classNames} from "./helpers/classNames/classNames";
-
+import {useTheme} from 'app/providers/ThemeProvider';
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 const App = () => {
@@ -18,8 +17,8 @@ const App = () => {
             <Link to={'/about'}>Про нас</Link>
             <Suspense fallback={<div>Завантаження...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageLazy/>}/>
-                    <Route path={'/'} element={<MainPageLazy/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
