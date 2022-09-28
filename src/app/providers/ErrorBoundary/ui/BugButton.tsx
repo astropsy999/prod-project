dc6d0056d6663663ad013340493ca81cd13d4f85
astropsy999 <bareust@gmail.com>
2022-09-28 12:39:41 +0300

    22 ErrorBoundary. Обработка React ошибок

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,24 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+// Компонент для тестування ErrorBoundary
+export const BugButton = () => {
+    const [error, setError] = useState(false);
+    const onThrow = () => setError(true);
+    const { t } = useTranslation();
+
+    useEffect(() => {
+        if (error) {
+            throw new Error();
+        }
+    }, [error]);
+    return (
+        <Button
+            onClick={onThrow}
+        >
+            {t('Помилка')}
+        </Button>
+    );
+};
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { BugButton } from 'app/providers/ErrorBoundary';
 
 function MainPage() {
     const { t } = useTranslation('mainpage');
     return (
         <div>
+            <BugButton />
             {t('Головна')}
         </div>
     );
