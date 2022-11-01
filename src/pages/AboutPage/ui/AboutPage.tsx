029fbbed01dd1f8a343d4c9154295d2a2fedc9eb
astropsy999 <bareust@gmail.com>
2022-11-01 22:32:24 +0300

    Кастомный Input. Окно авторизации. Lazy modal + stories

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Counter } from 'entities/Counter';
 
 function AboutPage() {
   const { t } = useTranslation('about');
-  return (
-    <div>
-      {t('Про нас')}
-      <Counter />
-    </div>
-  );
+  return <div>{t('Про нас')}</div>;
 }
 
 export default AboutPage;
