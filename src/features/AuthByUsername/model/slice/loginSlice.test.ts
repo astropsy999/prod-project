d92284110ac4eb9bf443d0b6f44ed7b466da1f8e
astropsy999 <bareust@gmail.com>
2022-11-18 08:02:01 +0300

    unit tests for loginSlice

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from 'features/AuthByUsername';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice.test', () => {
+  test('test username', () => {
+    const state: DeepPartial<LoginSchema> = { username: '123' };
+    expect(
+      loginReducer(state as LoginSchema, loginActions.setUsername('123123')),
+    ).toEqual({ username: '123123' });
+  });
+  test('test password', () => {
+    const state: DeepPartial<LoginSchema> = { password: '123' };
+    expect(
+      loginReducer(state as LoginSchema, loginActions.setPassword('123123')),
+    ).toEqual({ password: '123123' });
+  });
+});
