df1856a00bd6f6c4d92e62107d45c8f9a72e4ea3
astropsy999 <bareust@gmail.com>
2022-12-15 00:30:34 +0200

    fix closure error

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -13,9 +13,11 @@ export function useInfiniteScroll({
 }: UseInfiniteScrollOptions) {
   const observer = useRef<IntersectionObserver | null>(null);
   useEffect(() => {
+    const wrapperElement = wrapperRef.current;
+    const triggerElement = triggerRef.current;
     if (callback) {
       const options = {
-        root: wrapperRef.current,
+        root: wrapperElement,
         rootMargin: '0px',
         threshold: 1.0,
       };
@@ -25,12 +27,12 @@ export function useInfiniteScroll({
           callback();
         }
       }, options);
-      observer.current.observe(triggerRef.current);
+      observer.current.observe(triggerElement);
     }
 
     return () => {
-      if (observer.current) {
-        observer.current.unobserve(triggerRef.current);
+      if (observer.current && triggerElement) {
+        observer.current.unobserve(triggerElement);
       }
     };
   }, [callback, triggerRef, wrapperRef]);
