36ca72f4bed9cdfbd32f83a8b86a6788a792a776
astropsy999 <bareust@gmail.com>
2023-01-15 18:53:30 +0200

    85 Настраиваем Vite. Быстрая сборка для dev

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  plugins: [svgr({ exportAsDefault: true }), react()],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src/' }],
+  },
+  define: {
+    __IS_DEV__: JSON.stringify(true),
+    __API__: JSON.stringify('http://localhost:8000'),
+    __PROJECT__: JSON.stringify('frontend'),
+  },
+});
