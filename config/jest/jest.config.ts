311a4c5a7cb472cf8120962b0c51fc1836517ed6
astropsy999 <bareust@gmail.com>
2023-01-19 16:29:07 +0200

    98 Алиасы в jest. Fullscreen mode storybook

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -83,6 +83,7 @@ export default {
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
     '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    '^@/(.*)$': '<rootDir>src/$1',
   },
 
   reporters: [
