283a238c8e332a6d9874b129c5e75e623c0384c9
astropsy999 <bareust@gmail.com>
2023-01-19 21:10:26 +0200

    104 Улучшаем сборку. Babel loader cache. Postinstall hooks

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,16 +3,17 @@ import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 // @ts-ignore
 import CircularDependencyPlugin from 'circular-dependency-plugin';
 // @ts-ignore
-import CopyPlugin from 'copy-webpack-plugin';
-// @ts-ignore
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 // @ts-ignore
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 // @ts-ignore
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-// @ts-ignore
 import webpack from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+// @ts-ignore
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+// @ts-ignore
+import CopyPlugin from 'copy-webpack-plugin';
+
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -21,22 +22,17 @@ export function buildPlugins({
   apiUrl,
   project,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
+  const isProd = !isDev;
   const plugins = [
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
+
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project),
-    }),
-    new CopyPlugin({
-      patterns: [{ from: paths.locales, to: paths.buildLocales }],
     }),
 
     new CircularDependencyPlugin({
@@ -61,6 +57,20 @@ export function buildPlugins({
     plugins.push(
       new BundleAnalyzerPlugin({
         openAnalyzer: false,
+      }),
+    );
+  }
+
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css',
+      }),
+    );
+    plugins.push(
+      new CopyPlugin({
+        patterns: [{ from: paths.locales, to: paths.buildLocales }],
       }),
     );
   }
