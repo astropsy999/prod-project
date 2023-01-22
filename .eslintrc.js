10f849b25ea9b68f0113599f139cb59230e7371e
astropsy999 <bareust@gmail.com>
2023-01-22 15:22:25 +0200

    117 Prettier для форматирования кода. Интегрируем с eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,13 +18,13 @@ module.exports = {
     '@typescript-eslint',
     'i18next',
     'react-hooks',
-    // 'prettier',
+    'prettier',
     'paths-checking-plugin-ys',
     'unused-imports',
   ],
   rules: {
-    'react/jsx-indent': [2, 2],
-    'react/jsx-indent-props': [2, 2],
+    // 'react/jsx-indent': [2, 2],
+    // 'react/jsx-indent-props': [2, 2],
     indent: [2, 2, { SwitchCase: 1 }],
     'react/jsx-filename-extension': [
       2,
