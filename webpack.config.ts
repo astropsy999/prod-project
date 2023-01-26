0f581e003fc9c689e40df0bb7e49f2ec860699a4
astropsy999 <bareust@gmail.com>
2023-01-26 06:40:22 +0200

    getApiUrl function + bush script adjustment

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,18 @@
 import path from 'path';
 import webpack from 'webpack';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildPaths, BuildMode } from './config/build/types/config';
+
+function getApiUrl(mode: BuildMode, apiUrl?: string) {
+  if (apiUrl) {
+    return apiUrl;
+  }
+  if (mode === 'production') {
+    return '/api';
+  }
+
+  return 'http://localhost:8000';
+}
 
 export default (env: BuildEnv) => {
   const paths: BuildPaths = {
@@ -15,8 +26,7 @@ export default (env: BuildEnv) => {
 
   const mode = env?.mode || 'development';
   const PORT = env?.port || 3000;
-  const apiUrl = env?.apiUrl || 'http://localhost:8000';
-
+  const apiUrl = getApiUrl(mode, env?.apiUrl);
   const isDev = mode === 'development';
 
   const config: webpack.Configuration = buildWebpackConfig({
