6d065755429d02aa2164c8d72f4ae0bd36af6fab
astropsy999 <bareust@gmail.com>
2023-07-24 10:31:32 +0300

    shared/api/readme.md

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,14 +1,21 @@
+// Импорт необходимых зависимостей
 import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
 
+// Создание экземпляра axios с базовым URL, указанным в переменной окружения __API__
 export const $api = axios.create({
   baseURL: __API__,
 });
 
+// Интерцептор запросов axios для добавления заголовка Authorization
 $api.interceptors.request.use((config) => {
+  // Проверяем наличие заголовков в конфигурации запроса
   if (config.headers) {
+    // Получаем значение токена из localStorage по ключу USER_LOCALSTORAGE_KEY
+    // и устанавливаем его в заголовок Authorization
     config.headers.Authorization =
       localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
   }
+  // Возвращаем обновленную конфигурацию запроса
   return config;
 });
--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -1,18 +1,29 @@
+// Импорт необходимых зависимостей из библиотеки @reduxjs/toolkit/query/react
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage'; // Define a service using a base URL and expected endpoints
 
-// Define a service using a base URL and expected endpoints
+// Импорт константы USER_LOCALSTORAGE_KEY из файла с константами для работы с локальным хранилищем
+import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
+
+// Определение службы (service) с использованием базового URL и ожидаемых конечных точек (endpoints)
 export const rtkApi = createApi({
+  // Указываем путь до редьюсера (reducer), где будут храниться данные из запросов
   reducerPath: 'api',
+
+  // Определяем базовый запрос (baseQuery) с указанным базовым URL и функцией для подготовки заголовков
   baseQuery: fetchBaseQuery({
     baseUrl: __API__,
     prepareHeaders: (headers) => {
+      // Получаем токен из локального хранилища localStorage по ключу USER_LOCALSTORAGE_KEY
       const token = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+
+      // Если токен существует, добавляем его в заголовок Authorization
       if (token) {
         headers.set('Authorization', token);
       }
       return headers;
     },
   }),
+
+  // Определяем конечные точки (endpoints) для запросов (в данном случае пустой объект)
   endpoints: (builder) => ({}),
 });
